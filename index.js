9d6ac0aa01ab47e6c472ad03eefe1c6eaaba757c
Roberto Slawinski <robertoslawinski@gmail.com>
2025-05-07 19:59:06 +0100

    Data Type Lab

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
-/*******************************************
-    Iteration 1.1 | Tongue Twister
-*******************************************/
-const s1 = "Fred";
-const s2 = "fed";
-const s3 = "Ted";
-const s4 = "bread";
-const s5 = "and";
+// /*******************************************
+//     Iteration 1.1 | Tongue Twister
+// // *******************************************/
+// const s1 = "Fred";
+// const s2 = "fed";
+// const s3 = "Ted";
+// const s4 = "bread";
+// const s5 = "and";
 
-// Concatenate the string variables into one new string
-
-
-// Print out the concatenated string
+// // // Concatenate the string variables into one new string
+// const twister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s3 + " " + s2 + " " + s1 + " " + s4
 
 
+// // // // Print out the concatenated string
+
+// console.log(twister)
 
 
-/*******************************************
-    Iteration 1.2 | Camel Tail
-*******************************************/
-const part1 = "java";
-const part2 = "script";
+// // /*******************************************
+// //     Iteration 1.2 | Camel Tail
+// // *******************************************/
+// const part1 = "java";
+// const part2 = "script";
 
-// Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+// // // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+// const javA = part1.slice(0, -1) + part1.slice(-1).toLocaleUpperCase()
+// const scripT = part2.slice(0,-1) + part2.slice(-1).toLocaleUpperCase()
 
-
-// Print the cameLtaiL-formatted string
-
-
-
-
-/*******************************************
-    Iteration 2.1 | Calculate Tip
-*******************************************/
-const billTotal = 84;
-
-// Calculate the tip (15% of the bill total)
+// const javAscripT = javA + scripT
 
 
-// Print out the tipAmount
 
+// // // Print the cameLtaiL-formatted string
+
+// console.log(javAscripT)
+
+
+// // /*******************************************
+// //     Iteration 2.1 | Calculate Tip
+// // *******************************************/
+// const billTotal = 84;
+
+// // // Calculate the tip (15% of the bill total)
+
+// const tip = billTotal * 0.15
+
+// // // Print out the tipAmount
+
+// console.log(billTotal+tip)
 
 
 
@@ -46,31 +54,32 @@ const billTotal = 84;
     Iteration 2.2 | Generate Random Number
 *******************************************/
 
-// Generate a random integer between 1 and 10 (inclusive)
+// // Generate a random integer between 1 and 10 (inclusive)
 
+// const num = Math.floor(Math.random() * 10) + 1; 
+// // // Print the generated random number
 
-// Print the generated random number
+// console.log(num)
 
+// /*******************************************
+//     Iteration 3.1 | Booleans
+// *******************************************/
 
+// const a = true;
+// const b = false;
 
-/*******************************************
-    Iteration 3.1 | Booleans
-*******************************************/
-
-const a = true;
-const b = false;
-
-// Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
-
-const expression2 = a || b;
-
-const expression3 = !a && b;
-
-const expression4 = !(a && b);
-
-const expression5 = !a || !b;
-
-const expression6 = !(a || b);
-
-const expression7 = a && a;
+// // Try and guess the output of the below expressions first and write your answers down:
+// const expression1 = a && b;
+//FALSE 
+// const expression2 = a || b;
+//TRUE
+// const expression3 = !a && b;
+//FALSE
+// const expression4 = !(a && b);
+//TRUE
+// const expression5 = !a || !b;
+//TRUE
+// const expression6 = !(a || b);
+//FALSE
+// const expression7 = a && a;
+//FALSE
